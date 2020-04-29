06e1221dc2c326d8d90e2e7519e4b91f0740deeb
sbslc2000 <sbslc2000@gmail.com>
2020-04-29 14:54:06 +0900

    image를 지워봄

--- HeukSeok.js
+++ HeukSeok.js
@@ -650,19 +650,19 @@
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
         
-        for (var i = 0; i < positions.length; i ++) {
+    for(var i = 0; i < positions.length; i ++) {
         
-          var marker = new kakao.maps.Marker({
-                map: map, // 마커를 표시할 지도
-                position: positions[i].latlng, // 마커를 표시할 위치
-                /*if (positions.zeroavail === -1)
-                {
-                    image : markerRed;
-                }*/
-                image : markerImage,
-                title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-            });
+        var marker = new kakao.maps.Marker({
+               map: map, // 마커를 표시할 지도
+               position: positions[i].latlng, // 마커를 표시할 위치
+               /*if (positions.zeroavail === -1)
+               {
+                   image : markerRed;
+               }*/
+              //image : markerImage,
+              title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+          });
         
-          }
+       }
         
 
