ee1487b96d79db99bba2658d898ef03a90d7a014
sbslc2000 <sbslc2000@gmail.com>
2020-04-29 15:09:30 +0900

    크기와 좌표 수정

--- HeukSeok.js
+++ HeukSeok.js
@@ -644,8 +644,8 @@
         ];
 
     var imageSrc = './image/marker_red.png', // 마커이미지의 주소입니다    
-    imageSize = new kakao.maps.Size(30, 40), // 마커이미지의 크기입니다
-    imageOption = {offset: new kakao.maps.Point(20, 20)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+    imageSize = new kakao.maps.Size(25, 30), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(12.5, 0)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
       
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
