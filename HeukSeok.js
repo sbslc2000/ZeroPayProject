0137ccc53abea0c64b1ce0b4c3966603e40a575e
sbslc2000 <sbslc2000@gmail.com>
2020-04-29 14:47:07 +0900

    , 오타 수정

--- HeukSeok.js
+++ HeukSeok.js
@@ -648,7 +648,7 @@
     imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
       
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
         
         for (var i = 0; i < positions.length; i ++) {
         
